7a01073d6ca68a52c17fa0cc58d895128681727b
JESSICA FERIGATO MIGNELLA <Z081256@corp.clarobr>
2020-06-17 19:44:12 -0300

    novidade

---
+++ stock/src/test/java/com/migstok/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migstok.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
